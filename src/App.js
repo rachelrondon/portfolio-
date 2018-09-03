468e875db9a875d7dbf071bf7508af79aa204825
Rachel Rondon <rachel.sun.rondon@gmail.com>
2018-09-03 13:41:20 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <nav class="nav">
+            <div class="logo">
+              <h1 id="first">Rachel</h1>
+              <h2 id="last">Rondon</h2>
+            </div>
+          </nav>
+
+        </header>
+        <main>
+
+        </main>
+        <footer>
+
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default App;
